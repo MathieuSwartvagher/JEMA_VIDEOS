bc22391b9c7ac8398531e8ead80dabbc44811319
Swartvagher Mathieu <Mathieu.Swartvagher@enigma-school.com>
2021-10-20 14:39:15 +0200

    First commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,34 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBHB1wqBs1Kc0_jGoV8aRQCrV5427tqb_4",
+    authDomain: "jema-video.firebaseapp.com",
+    databaseURL: "https://jema-video-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "jema-video",
+    storageBucket: "jema-video.appspot.com",
+    messagingSenderId: "493492166576",
+    appId: "1:493492166576:web:7b39d3a618b4d0e54ac075"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = firebase.Firestore();
+
+
+
+window.addEventListener("load", () => {
+    getUsers();
+})
+
+function getUsers() {
+    db.collection("users").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.dir(doc.data());
+        });
+    });
+}
