4e2d8c9136ea0670702dcfaf783eee1ab7fd8ef4
Swartvagher Mathieu <Mathieu.Swartvagher@enigma-school.com>
2021-10-20 16:37:15 +0200

    feat: ajout select

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -13,16 +13,85 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-
+let selectBox = document.querySelector('#channelSelect');
 
 window.addEventListener("load", () => {
     getUsers();
+    getVideos();
 })
 
 function getUsers() {
     db.collection("users").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            console.dir(doc.data());
+            //console.dir(doc.data());
         });
     });
+}
+
+function getVideos() {
+    const docRef = db.collection("channel").doc("Liste_chaine");
+
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            const data = doc.data();
+
+            affectVideos(data.tableau);
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
+
+function affectVideos(listeChaines){
+    listeChaines.forEach(channel => {
+        selectBox.options.add(
+          new Option(channel, channel, false)
+        );
+    });
+}
+
+selectBox.addEventListener('change', () => {
+    fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyAebvd-2-p33hyYnFlWGG_CCB0ewB80AQ8&q=${selectBox.value}&part=snippet,id&maxResults=5`)
+    .then((resp) => {
+        return resp.json()
+    })
+    .then((data) => {
+        const videosListe = data
+        showVideos(videosListe.items);
+    })
+    .catch(() => {
+        // catch any errors
+    });
+});
+
+function showVideos(videosListe){
+    videosListe.forEach(video => {
+        console.dir(video.id.videoId)
+    });
+}
+
+function grid() {
+    var container = document.createElement('div');
+    container.id = "main";
+    container.className = "container";
+
+    for (var i = 0; i < 16; i++) {
+
+        var row = document.createElement('div');
+        row.className = "row";
+        row.id = "row" + i;
+
+        for (var j = 0; j < 16; j++) {
+            var box = document.createElement('div');
+            box.className = 'box';
+            row.appendChild(box);
+        }
+
+        container.appendChild(row);
+    }
+
+    return container;
 }
