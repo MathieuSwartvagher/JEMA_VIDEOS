cf957351e997bbf9bae45cabebac9119ca55fa7b
Swartvagher Mathieu <Mathieu.Swartvagher@enigma-school.com>
2021-10-21 10:31:54 +0200

    connection

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -14,8 +14,46 @@ const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 let selectBox = document.querySelector('#channelSelect');
+let pageVideo = document.querySelector("#pageVideo");
+let pageConnexion = document.querySelector("#connexion");
 
 window.addEventListener("load", () => {
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    var uiConfig = {
+        callbacks: {
+          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+          },
+          uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+          }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>',
+        // Privacy policy url.
+        privacyPolicyUrl: '<your-privacy-policy-url>'
+      };
+    
+      ui.start('#firebaseui-auth-container', uiConfig);
+
+    pageVideo.classList.add("hide");
     getUsers();
     getVideos();
 })
@@ -98,3 +136,46 @@ async function togglePictureInPicture() {
       }
     }
   }
+
+function createUser(){
+    let nom = document.querySelector('#nom').value;
+    let mdp = document.querySelector('#mdp').value;
+
+    firebase.auth().createUserWithEmailAndPassword(nom, mdp)
+    .then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ..
+    });
+}
+
+submit.addEventListener("submit", (event) => {
+    event.preventDefault();
+    connect();
+});
+
+function connect(){
+    let nom = document.querySelector('#nom').value;
+    let mdp = document.querySelector('#mdp').value;
+
+    firebase.auth().signInWithEmailAndPassword(nom, mdp)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+}
+
+
+
+
+
